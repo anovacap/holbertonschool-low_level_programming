4b6b1823c2bb647df35470ce8b8ac552f6764527
anovacap <anovacap@yahoo.com>
2018-06-20 20:41:45 +0000

    Fixed bugs 4_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * puts function
+ * main - starting function
+ *
+ * Return: nothing if susuccessful
 */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
