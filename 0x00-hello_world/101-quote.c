82889cdc04f52f09e00e452474b8ae83726945d2
anovacap <anovacap@yahoo.com>
2018-06-21 14:59:31 +0000

    Adding 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
