fd9d1aeccf9e1c8f25f6fe2ee52ccbb686236a04
anovacap <anovacap@yahoo.com>
2018-06-21 22:10:49 +0000

    Adding 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ *
+ * Return: nothing if successful
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
