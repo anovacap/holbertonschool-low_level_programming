72b8b7573c73dd5c3074e29ec14cfe05ffedf509
anovacap <anovacap@yahoo.com>
2018-06-21 05:31:34 +0000

    Adding 101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - start of the program
+ *
+ * Return: 1 stderr
+ */
+int main(void)
+{
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
