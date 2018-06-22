a2c8d78be9b55b8da49bc80a7cfe60747a9f1afe
anovacap <anovacap@yahoo.com>
2018-06-22 02:58:09 +0000

    Adding 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - start of the program
+ *
+ * Return: nothing if successful
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar (i + '0');
+			putchar (j + '0');
+			if (i == 9 && j == 8)
+				break;
+			putchar (',');
+			putchar (' ');
+		}
+		putchar ('\n');
+	}
+	return (0);
+}
