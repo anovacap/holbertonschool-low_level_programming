6a96ba8f790560dad1b0bd411c13e432ed429f5c
anovacap <anovacap@yahoo.com>
2018-06-25 21:50:02 +0000

    Initial commit 0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - starts the program
+ *
+ * Return: nothing if successful
+ */
+int main(void)
+{
+	char string[] = "Holberton";
+	int ind = 0;
+
+		while (string[ind])
+		{
+			_putchar(string[ind]);
+			ind++;
+		}
+	_putchar('\n');
+	return (0);
+}
