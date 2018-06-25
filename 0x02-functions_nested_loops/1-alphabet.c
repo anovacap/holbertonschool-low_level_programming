bb52a9e001463ebedb8c225a2e990dd927517be4
anovacap <anovacap@yahoo.com>
2018-06-25 22:28:10 +0000

    Initial commit 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - starts the program
+ *
+ * Return: nothing if successful
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
