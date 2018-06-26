9a6209d7dbb5f1acbe9eb516dc1b4a084b44b36e
anovacap <anovacap@yahoo.com>
2018-06-26 21:09:38 +0000

    fix bug

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
+
 /**
- * print-sign - prints the sign of a number
+ * print_sign - prints the sign of a number
  * @n: the charcter to be checked
  *
  * Return: sign(+,-,0) and (1,/,0) after check
