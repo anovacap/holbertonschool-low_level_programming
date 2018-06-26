b973083bb1f9a6c51acfc7c5294d0749d6215806
anovacap <anovacap@yahoo.com>
2018-06-26 20:16:31 +0000

    fix ascii in code

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,14 @@
 #include "holberton.h"
+
 /**
- * is_alpha - checks for alphabetic character
+ * _isalpha - checks for alphabetic character
  * @c: the charcter to be checked
  *
  * Return: 1 if charcter is alphabetic, 0 if not
  */
 int _isalpha(int c)
 {
-	int low;
-
-	if (c > 64 && c < 91 && c > 96 && c < 123)
-		low = 1;
-	else
-		low = 0;
-	return (low);
+	if (c >= 'A' && c < 'Z' && c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
 }
