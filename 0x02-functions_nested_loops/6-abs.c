a8e67de6bd1ee6b9686fc1c3872f320eba623624
anovacap <anovacap@yahoo.com>
2018-06-26 00:55:00 +0000

    Initial commit 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _abs - computes absolute value of an integer
+ * @i: the number to be computed
+ *
+ * Return: the absolute value of the integer
+ */
+int _abs(int i)
+{
+	int num;
+
+	if (i < 0)
+		num = i * -1;
+	else if (i > 0)
+		num = i;
+	else
+		num = 0;
+	return (num);
+}
