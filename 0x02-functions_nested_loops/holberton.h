7fcab21b3b2d3e06b587c3514c19dc33ff834d39
anovacap <anovacap@yahoo.com>
2018-06-26 00:54:17 +0000

    Initial commit holberton.h

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,17 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/**
+ * _putchar - writes the character c to stdout
+ * print_alphabet - prints the lowercase alphabet
+ * print_alphabet_x10 - prints lowercase alphabet 10x
+ * _islower - checks if character is lowercase
+ * _isalpha - checks is charcter is alphabetic
+ * print_sign - checks if number is + - 0
+ */
+void  _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void _islower(int c);
+void _isalpha(int c);
+void print_sign(int n);
+#endif 
