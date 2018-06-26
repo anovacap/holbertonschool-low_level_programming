ad8ca2c84e0073df580d2aeaa7876ae79ee6fa75
anovacap <anovacap@yahoo.com>
2018-06-26 20:24:51 +0000

    change prototype

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -17,7 +17,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int  _islower(int c);
 int  _isalpha(int c);
-void print_sign(int n);
+int  print_sign(int n);
 void _abs(int i);
 void print_last(int i);
 void jack_bauer(void);
