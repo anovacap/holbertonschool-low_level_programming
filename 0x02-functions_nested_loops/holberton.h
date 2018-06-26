828fdf0ed19c99597fb4c4cca838573c2336f4a9
anovacap <anovacap@yahoo.com>
2018-06-26 20:04:48 +0000

    change _islower

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -8,12 +8,18 @@
  * _isalpha - checks is charcter is alphabetic
  * print_sign - checks if number is + - 0
  * _abs - computes the absolute value of an integer
+ * print_last - prints the last digit of a number
+ * jack_bauer - prints minutes of day 00:00-24:59
+ * times_table - prints 9 times tables
  */
 void  _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-void _islower(int c);
+int  _islower(int c);
 void _isalpha(int c);
 void print_sign(int n);
 void _abs(int i);
+void print_last(int i);
+void jack_bauer(void);
+void times_table(void);
 #endif 
