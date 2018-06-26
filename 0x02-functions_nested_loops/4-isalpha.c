f3bf9ba35221ed8508fa25ea92022689755e5d45
anovacap <anovacap@yahoo.com>
2018-06-26 20:22:47 +0000

    fix && condition

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,7 +8,9 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'A' && c < 'Z' && c >= 'a' && c <= 'z')
+	if (c >= 'A' && c < 'Z')
+		return (1);
+	else if (c >= 'a' && c <= 'z')
 		return (1);
 	return (0);
 }
