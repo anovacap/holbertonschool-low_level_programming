67db8ed628a662984fba74469fc86372f0db946e
anovacap <anovacap@yahoo.com>
2018-06-26 21:30:09 +0000

    fix typo

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		_putchar('/');
+		_putchar('-');
 		return (-1);
 	}
 	else if (n == 0)
@@ -23,5 +23,4 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	_putchar('\n');
 }
