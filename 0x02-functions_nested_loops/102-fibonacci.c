79b789bcf6681bfda120fc3b32e3b46865aea14d
anovacap <anovacap@yahoo.com>
2018-06-28 00:20:04 +0000

    adding fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	int counter;
+	unsigned long fib;
+	int first = 0;
+	int second = 1;
+
+	for (counter = 0; counter < 51; counter++)
+	{
+		fib = first + second;
+		first = second;
+		second = fib;
+		printf("%lu", fib);
+		if (counter < 50)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
