83ceef0395cbbd56577e9bf8af46ad3739f531b8
anovacap <anovacap@yahoo.com>
2018-07-02 22:47:51 +0000

    first add 1-swap.c and 2-strlen.c

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
