fd31c2651b4749d3966c912c26ebe0ced6d243fe
anovacap <anovacap@yahoo.com>
2018-07-02 23:32:59 +0000

    add 3-puts.c

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
