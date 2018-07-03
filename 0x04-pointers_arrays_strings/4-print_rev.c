c76a86f99064b76c155f43ecefff99e27a756be8
anovacap <anovacap@yahoo.com>
2018-07-03 00:09:26 +0000

    add 4-print_rev.c

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string to be reversed
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i;
+	int x;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (x = i; x >= 0; x--)
+		_putchar(s[x]);
+	_putchar('\n');
+}
