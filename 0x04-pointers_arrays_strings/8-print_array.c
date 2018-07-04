e6d58ef4defb5b83e991848143426cf46d98232f
anovacap <anovacap@yahoo.com>
2018-07-04 01:08:56 +0000

    add 8

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array
+ * @n: integer
+ *
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		printf("%d, ", a[i]);
+	printf("\n");
+}
