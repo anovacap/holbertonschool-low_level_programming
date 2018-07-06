4fa2fadb73aafabd296d16711b554f47e6d6eacc
anovacap <anovacap@yahoo.com>
2018-07-06 16:15:51 +0000

    fix counter bug

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -19,13 +19,13 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			count++;
+			if (count == 0)
+				return (0);
 			num = num * 10 + (s[i] - '0');
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 		}
 	}
-	if (count == 0)
-		return (0);
 	if (sign % 2 == 0)
 		sign = 1;
 	else
