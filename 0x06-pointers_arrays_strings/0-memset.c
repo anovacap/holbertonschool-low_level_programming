19c715800bb71933e347bbd946b75d10a17fc8f1
anovacap <anovacap@yahoo.com>
2018-07-10 16:43:07 +0000

    initial commit ex.0

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: char array to be updated
+ * @b: char goes to s
+ * @n: unsigned int times s updated
+ *
+ * Return - updated s char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
