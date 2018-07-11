e86e2fdab3ec06480fe08c9a33742169b86f0d34
anovacap <anovacap@yahoo.com>
2018-07-11 18:09:38 +0000

    add all prototypes

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -54,4 +54,11 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
 #endif
