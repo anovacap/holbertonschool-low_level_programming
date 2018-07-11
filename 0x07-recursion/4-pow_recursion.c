741e01da8b87edfe43d4b01ec39a6f98f0837976
anovacap <anovacap@yahoo.com>
2018-07-11 23:12:42 +0000

    add ex.4

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: int 1
+ * @y: int 2
+ *
+ * Return: the powers
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return ((x) * _pow_recursion(x, y - 1));
+	else if
+		(y == 0)
+		return (1);
+	else
+		return (-1);
+}
