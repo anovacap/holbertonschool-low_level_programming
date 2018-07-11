3d6d5b7e40fd5e1b91e6c7ce14f09bfad0f584f0
anovacap <anovacap@yahoo.com>
2018-07-11 17:58:19 +0000

    new .h

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -1,59 +1,6 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-/**
- * _putchar - writes the character c to stdout
- * print_alphabet - prints the lowercase alphabet
- * print_alphabet_x10 - prints lowercase alphabet 10x
- * _islower - checks if character is lowercase
- * _isalpha - checks is charcter is alphabetic
- * print_sign - checks if number is + - 0
- * _abs - computes the absolute value of an integer
- * print_last_digit - prints the last digit of a number
- * jack_bauer - prints minutes of day 00:00-24:59
- * times_table - prints 9 times tables
- * add - adds 2 numbers together
- * print_to_98 - prints numbers from n to 98
- * print_times_table - prints n times table, start at 0
- * _isupper - checks for uppercase character
- * _isdigit - checks for a digit (0 through 9)
- * mul - multiplies two integers
- * print_numbers - prints the numbers, from 0 to 9, followed by a new line
- * print_most_numbers - prints from 0 to 9, no 2 0r 4 followed by a new line
- * more_numbers - prints 10 times the numbers, from 0 to 14
- * print_line - draws a straight line in the terminal
- * print_diagonal - draws a diagonal line on the terminal
- * print_square - prints a square, followed by a new line
- * print_triangle - prints a triangle, followed by a new line
- * reset_to_98 - takes pointer and updates it to 98
- * swap_int - swaps the values of two integers
- * _strlen - returns the length of a string
- * _puts - prints a string, followed by a new line, to stdout
- * print_rev - prints a string, in reverse
- * rev_string - function that reverses a string
- * puts2 - prints one char out of 2 of a string
- * puts_half - prints half of a string
- * print_array - prints n elements of an array of integers
- * _strcpy - copies the string pointed to src to the buffer pointed to dest
- * _strcat - concatenates two strings
- * _strncat - concatenates two strings
- * _strncpy - copies a string
- * _strcmp - compares two strings
- * reverse_array - reverses the content of an array of integers
- * string_toupper - lowercase letters of a string to uppercase
- * cap_string - capitalizes all words of a string
- * leet - encodes a string into 1337 LEET
- * rot13 - encodes a string using rot13
- * _memset - fills memory with a constant byte
- * _memcpy - copies memory area
- * _strchr - locates a character in a string
- * _strspn - gets the length of a prefix substring
- * _strpbrk - searches a string for any of a set of bytes
- * _strstr - locates a substring
- * print_chessboard - prints the chessboard
- * print_diagsums - sum of the two diagonals of a square matrix of integers
- * set_string - sets the value of a pointer to a char
- * _puts_recursion - prints a string, followed by a new line
- */
+
 void  _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
