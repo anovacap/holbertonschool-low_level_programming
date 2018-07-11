29b0c86a5b3b7f4f2ac1578f7d22b75a6bde3f71
anovacap <anovacap@yahoo.com>
2018-07-11 22:11:20 +0000

    add ex2

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to be measured
+ *
+ * Return: integer length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1) + _strlen_recursion(s + 1);
+	return (0);
+}
