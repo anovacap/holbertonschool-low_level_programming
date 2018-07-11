5682e7680cdb1f18fbffb7f057cb8288de1206ca
anovacap <anovacap@yahoo.com>
2018-07-11 17:45:52 +0000

    remove return add else

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -10,10 +10,10 @@ void _puts_recursion(char *s)
 {
 
 	if (s[0] == '\0')
-	{
 		_putchar('\n');
-		return;
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
 	}
-	_putchar(s[0]);
-	_puts_recursion(s + 1);
 }
