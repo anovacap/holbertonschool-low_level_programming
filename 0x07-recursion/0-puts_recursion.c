b4b9b271c80b2c2185286046ec57bf3291e0748b
anovacap <anovacap@yahoo.com>
2018-07-11 17:37:09 +0000

    first commit

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+
+	if (s[0] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
