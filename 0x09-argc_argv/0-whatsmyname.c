5bf8e4ddc63ff356c8834cd845b5208147b256c7
anovacap <anovacap@yahoo.com>
2018-07-13 23:39:39 +0000

    add ex.0

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: count of command line arguments
+ * @argv: arguments
+ *
+ * Return: integer
+ */
+int main(int argc, char* argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
