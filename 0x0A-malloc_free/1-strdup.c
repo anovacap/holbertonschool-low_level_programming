4e11554a9f49efbfc3b4e425cd62464768d82406
anovacap <anovacap@yahoo.com>
2018-07-17 19:08:30 +0000

    fix memory bug

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -10,17 +10,17 @@
 char *_strdup(char *str)
 {
 	char *dup;
-	int i, k;
-	int size = sizeof(str);
+	int i;
+	int k = 0;
 
 	for (k = 0; str[k] != '\0'; k++)
 		;
 	if (k == 0)
 		return (NULL);
-	dup = malloc(sizeof(*dup * k) + 1);
-	if (sizeof(dup) == 0)
+	dup = (char *)malloc(sizeof((*dup) * k) + 1);
+	if (dup == 0)
 		return (NULL);
-	for (i = 0; i <= size; i++)
+	for (i = 0; i <= k; i++)
 		dup[i] = str[i];
 	return (dup);
 }
