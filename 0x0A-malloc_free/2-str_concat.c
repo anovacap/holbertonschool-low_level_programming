c57ffc7d752e81df2e234a9ae59f97238ce5cb91
anovacap <anovacap@yahoo.com>
2018-07-17 22:42:21 +0000

    fix if checks

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -48,12 +48,12 @@ char *str_concat(char *s1, char *s2)
 	int s2len = _strlen(s2);
 	int s1ands2 = s1len + s2len;
 
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
 	if (s1 != NULL && s2 != NULL)
 		cat = malloc(sizeof(char) * s1ands2  + 1);
-	if (s1 == NULL)
-		return (_strcpy(cat, s2));
-	else if (s2 == NULL)
-		return (_strcpy(cat, s1));
 	_strcpy(cat, s1);
 	for (i = 0; s2[i] != '\0'; i++)
 		cat[s1len++] = s2[i];
