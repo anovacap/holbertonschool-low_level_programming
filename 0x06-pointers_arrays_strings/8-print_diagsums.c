44db9d38a55fd5087260f05bbb99979592caa5ec
anovacap <anovacap@yahoo.com>
2018-07-22 15:03:32 +0000

    add ex.8

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -12,11 +12,10 @@ void print_diagsums(int *a, int size)
 	int i;
 	int sum1 = 0, sum2 = 0;
 
-
 	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + (i * size + 1));
-		sum2 += *(a + (i * size + size - i - 1));
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + size - i - 1);
 	}
 	printf("%i, %i\n", sum1, sum2);
 }
