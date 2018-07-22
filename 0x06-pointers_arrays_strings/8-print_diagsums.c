2cd66625f9eb829c9d2dd673cf5a83abad1fd62a
anovacap <anovacap@yahoo.com>
2018-07-22 14:54:09 +0000

    add ex.8

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sum of the two diagonals of a square matrix of integers
+ * @a: first integer (row)
+ * @size: second integer (column)
+ * Return: nothing - output to screen
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0, sum2 = 0;
+
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + (i * size + 1));
+		sum2 += *(a + (i * size + size - i - 1));
+	}
+	printf("%i, %i\n", sum1, sum2);
+}
