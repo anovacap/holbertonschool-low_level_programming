f16e2d4bb0969f2715791c5c399638d361e0500d
anovacap <anovacap@yahoo.com>
2018-07-23 22:00:37 +0000

    update .h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @owner: char (owner)
  * Description: create first struct of a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
