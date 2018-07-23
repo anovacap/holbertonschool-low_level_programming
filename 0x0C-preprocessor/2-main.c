5806a0833da7c1efabc386785816c8a033e94e88
anovacap <anovacap@yahoo.com>
2018-07-23 16:41:05 +0000

    add ex.2

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+#ifndef __BASE_FILE__
+#define __BASE_FILE__
+#endif
+
+/**
+ * main - print file name
+ * Return: nothing if success
+ */
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
