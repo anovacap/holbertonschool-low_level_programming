b000bbec730b752bc60ab372e5023c6d436dd114
anovacap <anovacap@yahoo.com>
2018-07-23 20:46:21 +0000

    add ex.2

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: initializes dog struct
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: nil\n");
+		else if (d->owner == NULL)
+			printf("Owner: nil\n");
+		else
+		{
+			printf("Name: %s\n", d->name);
+			printf("Age: %f\n", d->age);
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+	exit (0);
+}
