d43b0daea7f9997e6a704c1b98ddcb73f3aea2aa
anovacap <anovacap@yahoo.com>
2018-07-23 21:07:52 +0000

    remove NULL check

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -12,17 +12,12 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d != NULL)
-	{
-		if (d->name == NULL)
-			printf("Name: (nil)\n");
-		else if (d->owner == NULL)
-			printf("Owner: (nil)\n");
-		else
-		{
-			printf("Name: %s\n", d->name);
-			printf("Age: %f\n", d->age);
-			printf("Owner: %s\n", d->owner);
-		}
-	}
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
