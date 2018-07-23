4f7f834b89bd2fbaa42b77cfbc1bfca8d37fdca7
anovacap <anovacap@yahoo.com>
2018-07-23 19:59:31 +0000

    add NULL check

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * init_dog - initialize a variable of type struct dog
  * @d: struct dog set to d
@@ -11,6 +11,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d != NULL)
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
