47ab1388b66f2f395cc1a4903d1ea0e8f1f2745c
anovacap <anovacap@yahoo.com>
2018-07-27 21:28:24 +0000

    add ex.2

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings, followed by a new line
+ * @separator: string to be printed
+ * @n: number of strings passed
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	if (!n)
+		return;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		char *str = va_arg(args, char*);
+
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+}
