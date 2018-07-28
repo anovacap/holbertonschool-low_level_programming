8e42690a117316fa0982a72d95796892605ada4e
anovacap <anovacap@yahoo.com>
2018-07-28 22:46:48 +0000

    add _strlen prototype

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,12 +1,12 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
-#include <stddef.h>
 #include <stdarg.h>
 #include <stdio.h>
-#include <string.h>
+
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+unsigned int _strlen(const char *s);
 #endif
