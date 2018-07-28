b20ee96f94d238fd44622d1e0c709a02835251f1
anovacap <anovacap@yahoo.com>
2018-07-28 21:07:14 +0000

    fix print format

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -11,9 +11,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	va_start(args, n);
-	for (i = 0; i < n; i++)
-		printf("%s%d", separator, va_arg(args, int));
-	printf("\n");
-	va_end(args);
+	if (separator)
+	{
+		va_start(args, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(args, int));
+			if (i < n - 1)
+				printf("%s", separator);
+		}
+		printf("\n");
+		va_end(args);
+	}
 }
