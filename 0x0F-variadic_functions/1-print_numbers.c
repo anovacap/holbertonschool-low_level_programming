f228d9c607964720832818b1a9af7714cb0b31d1
anovacap <anovacap@yahoo.com>
2018-07-28 21:15:58 +0000

    check for separator

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -11,16 +11,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (separator)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(args, n);
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(args, int));
-			if (i < n - 1)
-				printf("%s", separator);
-		}
-		printf("\n");
-		va_end(args);
+		printf("%d", va_arg(args, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
 	}
+	printf("\n");
+	va_end(args);
 }
