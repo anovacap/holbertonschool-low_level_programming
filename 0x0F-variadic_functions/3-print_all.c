f47fe7a1de38aa5beb8b7f0d22a9dd85e6026643
anovacap <anovacap@yahoo.com>
2018-07-28 02:06:02 +0000

    add ex.2 ex.3

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: type arguments
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0;
+	int x = 0;
+	va_list args;
+	char *st;
+
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 's':
+			st = va_arg(args, char*);
+
+			if (st == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", st);
+			break;
+		default:
+			x++;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+		     || format[i] == 's') && (i < strlen(format) - x))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
