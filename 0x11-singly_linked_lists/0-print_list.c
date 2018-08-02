443980e5421a30b262e827d78924654a87d90cfb
anovacap <anovacap@yahoo.com>
2018-08-02 18:23:51 +0000

    add ex.0

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: struct list_t
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			count++;
+			h = h->next;
+			printf("[%x] %s\n", h->len, h->str);
+			count++;
+			break;
+		}
+
+		printf("[%x] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+
+	}
+	return (count);
+}
