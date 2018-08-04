4be7d98cf6a437fa29b4b785c09bfccaf4b2ca65
anovacap <anovacap@yahoo.com>
2018-08-04 00:10:23 +0000

    add ex.100

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+void myStartupFun (void) __attribute__ ((constructor));
+void myStartupFun (void)
+{
+	printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
