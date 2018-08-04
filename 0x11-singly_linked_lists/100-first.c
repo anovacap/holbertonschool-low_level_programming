1f716f5f95f733a7d139207dfdaac02d806c76e3
anovacap <anovacap@yahoo.com>
2018-08-04 01:26:15 +0000

    remove errant space

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
  */
 void tortoise(void)
 {
-	printf("You're beat! and yet, you must allow,\n ");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
