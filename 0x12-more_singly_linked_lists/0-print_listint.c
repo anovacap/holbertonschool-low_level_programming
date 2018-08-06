ed61b9f6423f7719b0ce32b922749720305675b3
anovacap <anovacap@yahoo.com>
2018-08-06 14:15:40 +0000

    add ex.0

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t
+ * @h: struct listint_t
+ * Return: size_t number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		if (h->n == '\0')
+			printf("0");
+		else
+			printf("%i\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
