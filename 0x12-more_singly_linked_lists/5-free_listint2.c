c0b9f3388ad377c581cb36c53bc3bdbe5f3130fb
anovacap <anovacap@yahoo.com>
2018-08-07 18:33:52 +0000

    set current to *head below NULL check

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -7,16 +7,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *current;
 	listint_t *next;
+
 	if (head == NULL)
 		return;
-	while (current  != NULL)
+	current = *head;
+	while (current != NULL)
 	{
 		next = current->next;
 		free(current);
 		current = next;
 	}
-	free(current);
 	*head = NULL;
 }
