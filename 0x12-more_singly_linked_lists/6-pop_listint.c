64f51f6cc321845bdb811b8e4deec1a539c1c9bb
anovacap <anovacap@yahoo.com>
2018-08-07 18:53:28 +0000

    add check for *head

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -6,11 +6,11 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *new;
+	listint_t *new = NULL;
 	int x = (*head)->n;
 
 	new = *head;
-	if (!head)
+	if (!head || !(*head))
 		return (0);
 	*head = (*head)->next;
 	free(new);
