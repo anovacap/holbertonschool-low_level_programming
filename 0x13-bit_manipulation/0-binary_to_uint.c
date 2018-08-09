8dba8f891193eef920038e3e7885ef204bf447ec
anovacap <anovacap@yahoo.com>
2018-08-09 20:15:36 +0000

    add ex.0

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+	for(i = 0; b[i] != '\0'; i++)
+		if (b[i]  != '0' && b[i] != '1')
+			return (0);
+	for(i = 0; b[i] != '\0'; i++)
+	{
+		num *= 2;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
+}
