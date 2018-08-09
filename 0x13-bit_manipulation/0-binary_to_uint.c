1c885fca2bcfa0b6c4417dc7032e8c4bfb695caf
anovacap <anovacap@yahoo.com>
2018-08-09 20:16:30 +0000

    add ex.0

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -12,10 +12,10 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	for(i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 		if (b[i]  != '0' && b[i] != '1')
 			return (0);
-	for(i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		num *= 2;
 		if (b[i] == '1')
