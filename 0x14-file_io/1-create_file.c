a3ca4493fd86467311002dbfc24166646e23d5c5
anovacap <anovacap@yahoo.com>
2018-08-14 22:56:09 +0000

    add check for malloc

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,9 +13,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	txt = malloc(sizeof(text_content));
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
+		return (-1);
+	txt = malloc(sizeof(text_content));
+	if (!txt)
 		return (-1);
 	write(fd, text_content, sizeof(txt));
 	free(txt);
