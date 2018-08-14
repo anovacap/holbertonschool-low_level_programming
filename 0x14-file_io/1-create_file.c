7158b6e7330e7e17678c31ba237b58a6a2efc78d
anovacap <anovacap@yahoo.com>
2018-08-14 23:57:12 +0000

    add return

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -19,6 +19,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 		close(fd);
+		return (1);
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
