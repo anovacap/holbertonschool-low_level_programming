0a6661dc1c3a12d5e5e63b487975b021823a75d5
anovacap <anovacap@yahoo.com>
2018-08-15 00:05:32 +0000

    add NULL byte

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -11,8 +11,6 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int i;
 
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
@@ -21,10 +19,12 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	write(fd, text_content, i);
+	write(fd, text_content, i + 1);
 	close(fd);
 	return (1);
 }
