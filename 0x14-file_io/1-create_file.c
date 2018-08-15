241f891aeb425eb55c20b8ecb85840a4d356a289
anovacap <anovacap@yahoo.com>
2018-08-15 00:08:48 +0000

    remove NULL byte

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	write(fd, text_content, i + 1);
+	write(fd, text_content, i);
 	close(fd);
 	return (1);
 }
