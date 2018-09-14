1d0c9b170b396c1f8bbd75eda8e334b6cbfa71c1
anovacap <anovacap@yahoo.com>
2018-09-14 20:05:15 +0000

    remove mover

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -7,13 +7,11 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *mover = (dlistint_t *)h;
 	size_t i = 0;
 
 	while (h != NULL)
 	{
 		printf("%i\n", h->n);
-		*mover = *h;
 		h = h->next;
 		i++;
 	}
