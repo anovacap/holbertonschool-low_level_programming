7b7cba4a09004fb935df847c22e3c2fe4aa541d1
anovacap <anovacap@yahoo.com>
2018-09-14 17:43:12 +0000

    Initial commit add ex.4

---
+++ 0x16-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Free a dlistint_t list.
+ * @head: start of doub ll.
+ * Return: Nothing if success.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head)
+		free(head);
+}
