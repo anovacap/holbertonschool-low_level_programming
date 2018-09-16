c8cb44ead1c04b0b80ed1242df34e39d2b83f260
anovacap <anovacap@yahoo.com>
2018-09-16 18:21:13 +0000

    no idx return add_dnodeint

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,19 +25,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx > c)
 		return (NULL);
 	mover = *h;
-	/*if (idx == c)
-	{
-		new_dl->next = NULL;
-		new_dl->prev = mover;
-		mover->next = new_dl;
-		}*/
 	if (!idx)
-	{
-		new_dl->next = *h;
-		*h = new_dl;
-		new_dl->prev = NULL;
-		return (new_dl);
-	}
+		return (add_dnodeint(h, n));
 	for (x = 1; x < idx; x++)
 		mover = mover->next;
 	new_dl->next = mover->next;
