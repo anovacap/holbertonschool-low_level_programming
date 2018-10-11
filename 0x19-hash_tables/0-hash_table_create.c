35f96bb30c0eb3a1ca69cfa4081a49540b4334d4
anovacap <anovacap@yahoo.com>
2018-10-11 20:28:22 +0000

    Initial commit

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: Size of the array
+ * Return: Returns a pointer to the newly created hash table or NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+	unsigned long int i = 0;
+
+	if (size < i)
+		return (NULL);
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (new->array == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		new->array[i] = NULL;
+		i++;
+	}
+	return (new);
+}
