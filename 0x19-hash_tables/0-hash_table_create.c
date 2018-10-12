1aac79f5cba05ff4afa097f821f96fd84814403f
anovacap <anovacap@yahoo.com>
2018-10-12 00:26:46 +0000

    set size and free(new)

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -15,9 +15,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 		return (NULL);
+	new->size = size;
 	new->array = malloc(sizeof(hash_node_t *) * size);
 	if (new->array == NULL)
+	{
 		return (NULL);
+		free(new);
+	}
 	while (i < size)
 	{
 		new->array[i] = NULL;
