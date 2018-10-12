4421df99b1997ce319624755d672aac9468bc7ac
anovacap <anovacap@yahoo.com>
2018-10-12 06:04:13 +0000

    add prototype

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -31,7 +31,7 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
-
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value);
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
