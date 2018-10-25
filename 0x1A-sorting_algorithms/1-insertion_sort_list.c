80c5630e8d7dbdaa995527bace938f3662feb8e8
anovacap <anovacap@yahoo.com>
2018-10-25 20:46:19 +0000

    initial commit

---
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - insertion sort algo
+ * @list: listint_t type
+ * Return: Nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *ptr = NULL, *wlk = NULL;
+
+	if (!(*list)->next)
+		return;
+	ptr = wlk = (*list)->next;
+	while (wlk)
+	{
+		wlk = wlk->next;
+		while (ptr->prev && ptr->n < ptr->prev->n)
+		{
+			ptr->prev->next = ptr->next;
+			if (ptr->next)
+				ptr->next->prev = ptr->prev;
+			ptr->next = ptr->prev;
+			ptr->prev = ptr->next->prev;
+			if (ptr->prev)
+				ptr->prev->next = ptr;
+			else
+				*list = ptr;
+			ptr->next->prev = ptr;
+			print_list(*list);
+		}
+		ptr = wlk;
+	}
+}
