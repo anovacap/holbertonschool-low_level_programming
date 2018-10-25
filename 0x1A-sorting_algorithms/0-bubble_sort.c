059e02002e2b4aabf04324e45b49261c7a42a655
anovacap <anovacap@yahoo.com>
2018-10-25 16:12:02 +0000

    check for size < 2

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -10,6 +10,8 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
+	if (size < 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 		for (j = 0; j < size - 1; j++)
 			if (array[j] > array[j + 1])
