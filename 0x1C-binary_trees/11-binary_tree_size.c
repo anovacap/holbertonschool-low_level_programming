9f38fb8afb9344b889d1ae6869642c7f664f77b3
anovacap <anovacap@yahoo.com>
2018-12-11 20:54:14 +0000

    refactor code

---
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to traverse
+ * Return: size_t size of tree or 0 if no tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
