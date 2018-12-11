c23406e984a88796e2534293e881b6e69f209943
anovacap <anovacap@yahoo.com>
2018-12-11 01:41:34 +0000

    line length fix for betty format

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right -  inserts a node as the right-child of another node
+ * binary_tree_insert_right - inserts a node as the right-child of another node
  * @parent: pointer to the node to insert the right-child in
  * @value: the value to store in the new node
  * Return: pointer to the created node, or NULL if parent is NULL or fail
