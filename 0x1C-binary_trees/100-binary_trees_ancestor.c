0f3ae821c578775df5e9d4d9f32e31fd7bd1ae6a
Alex Farioletti <gsf.nyc@gmail.com>
2018-12-12 15:16:01 -0800

    reset s_run

---
+++ 0x1C-binary_trees/100-binary_trees_ancestor.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: pointer to the lowest common ancestor
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	binary_tree_t *f_run = (binary_tree_t *)first;
+	binary_tree_t *s_run = (binary_tree_t *)second;
+
+	if (!first->parent || !second->parent)
+		return (NULL);
+	while (f_run->parent != NULL)
+	{
+		while (s_run->parent != NULL)
+		{
+			printf("s_run = %i\n", s_run->n);
+			if (f_run == s_run)
+				break;
+			s_run = s_run->parent;
+		}
+		if (f_run == s_run)
+			break;
+		f_run = f_run->parent;
+		s_run = (binary_tree_t *)second;
+	}
+	return (f_run);
+}
